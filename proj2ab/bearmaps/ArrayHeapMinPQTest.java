f76fe7458bfc01bcecb79d669445d157650aab21
Hui528 <huiw0528@gmail.com>
2021-12-21 20:55:46 -0800

    KdTree and extrinsicMinPQ update

--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,120 @@
 package bearmaps;
 
-import org.junit.Test;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class ArrayHeapMinPQTest {
+    public static void main(String[] args) {
+        //ArrayHeapMinPQ<Character> ex1 = example1();
+        //ArrayHeapMinPQ<Integer> ex2 = example2(100);
+        //ex2.printMinHeap();
+        //addAllItems(minHeapList);
+        //getSmallestTest(minHeapList);
+        //removeSmallestTest(minHeapList, 10);
+        //sizeTest(minHeapList);
+        //changePriorityTest(ex1, 'k', 7);
+        //changePriorityTest(ex1, 'y', 0);
+        //addTest(minHeapList, 'a', 1.5);
+        //addTest(minHeapList, 'd', 5.5);
+        //containsTest(minHeapList, 'y');
+        addTimeTable(1000000, 25000, 8);
+    }
+
+    private static ArrayHeapMinPQ<Character> example1() {
+        ArrayHeapMinPQ<Character> minHeapList = new ArrayHeapMinPQ<>();
+        minHeapList.add('b', 3);
+        minHeapList.add('e', 1);
+        minHeapList.add('g', 4);
+        minHeapList.add('k', 0);
+        minHeapList.add('p', 5);
+        minHeapList.add('v', 2);
+        minHeapList.add('y', 6);
+        return minHeapList;
+    }
+
+    private static ArrayHeapMinPQ<Integer> example2(int size) {
+        Random random = new Random();
+        ArrayHeapMinPQ<Integer> minHeapList = new ArrayHeapMinPQ<>();
+        for(int i = 0; i < size; i++) {
+            minHeapList.add(i, random.nextDouble());
+        }
+        return minHeapList;
+    }
+
+    private static void printTimingTable(List<Integer> Ns, List<Double> times, List<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    private static void addTimeTable(int heapSize, int baseSize, int addTimes) {
+        List<Integer> Ns = new ArrayList<>();
+        List<Double> times = new ArrayList<>();
+        List<Integer> opCounts = new ArrayList<>();
+        for(int i = 0; i < addTimes; i++) {
+            int size = baseSize * (int)Math.pow(2, i);
+            Ns.add(size);
+            opCounts.add(size);
+            times.add(addTime(heapSize, size));
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    private static double addTime(int size, int addTimes) {
+        Random ranXY = new Random();
+        ArrayHeapMinPQ<Integer> ex2 = example2(size);
+        Stopwatch sw = new Stopwatch();
+        for(int i = 0; i < addTimes; i++) {
+            int nextItem = ranXY.nextInt();
+            while(ex2.contains(nextItem)) {
+                nextItem = ranXY.nextInt();
+            }
+            ex2.add(nextItem, ranXY.nextDouble());
+        }
+        double timeInSeconds = sw.elapsedTime();
+        return timeInSeconds;
+    }
+
+    private static void getSmallestTest(ArrayHeapMinPQ<Character> minHeapList) {
+        System.out.println("The smallest item in this min heap is " + minHeapList.getSmallest());
+    }
+
+    private static void removeSmallestTest(ArrayHeapMinPQ<Character> minHeapList, int times) {
+        int size = minHeapList.size();
+        System.out.println("Remove smallest item " + times + " times: ");
+        for(int i = 0; i < Math.min(times, size); i++) {
+            System.out.println(minHeapList.removeSmallest());
+        }
+    }
+
+    private static void sizeTest(ArrayHeapMinPQ<Character> minHeapList) {
+        System.out.println("The size of min heap is " + minHeapList.size());
+    }
+
+    private static void changePriorityTest(ArrayHeapMinPQ<Character> minHeapList, char item, double priority) {
+        System.out.println("Changing item " + item + ", new priority is " + priority);
+        minHeapList.changePriority(item, priority);
+        System.out.println("After changing priority, list items:");
+        minHeapList.printMinHeap();
+    }
+
+    private static void addTest(ArrayHeapMinPQ<Character> minHeapList, char item, double priority) {
+        System.out.println("Add new item " + item + ", priority " + priority);
+        minHeapList.add(item, priority);
+        System.out.println("After add new item, list items:");
+        minHeapList.printMinHeap();
+    }
+
+    private static void containsTest(ArrayHeapMinPQ<Character> minHeapList, char item) {
+        System.out.println("Item " + item + " exists in this min heap?  " + minHeapList.contains(item));
+    }
 }
--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -1,7 +1,168 @@
 package bearmaps;
 
-import org.junit.Test;
+import edu.princeton.cs.algs4.Stopwatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class KDTreeTest {
+
+    public static void main(String[] args) {
+        System.out.println("Timing table for Kd-Tree Construction");
+        constructionTime(31250);
+        System.out.println("Timing table for Naive Nearest");
+        naiveNearestTime(125);
+        System.out.println("Timing table for Kd-Tree Nearest");
+        KDTNearestTime(31250);
+        System.out.println("Test finding nearest point by Naive Nearest and Kd-Tree Nearest");
+        findNearestP();
+
+    }
+
+    private static void printTimingTable(List<Integer> Ns, List<Double> times, List<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+    private static void constructionTime(int baseSize) {
+        List<Integer> Ns = new ArrayList<>();
+        List<Double> times = new ArrayList<>();
+        List<Integer> opCounts = new ArrayList<>();
+        for(int i = 0; i < 7; i++) {
+            int size = baseSize * (int)Math.pow(2, i);
+            Ns.add(size);
+            opCounts.add(size);
+            times.add(constructKDTree(size));
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    private static Double constructKDTree(int size) {
+        Random ranXY = new Random();
+        ArrayList<Point> KDTlist = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            KDTlist.add(new Point(ranXY.nextDouble(), ranXY.nextDouble()));
+        }
+        Stopwatch sw = new Stopwatch();
+        KDTree tree = new KDTree(KDTlist);
+        double timeInSeconds = sw.elapsedTime();
+        return timeInSeconds;
+    }
+
+    private static void naiveNearestTime(int baseSize) {
+        int ops = 1000000;
+        List<Integer> Ns = new ArrayList<>();
+        List<Double> times = new ArrayList<>();
+        List<Integer> opCounts = new ArrayList<>();
+        for(int i = 0; i < 4; i++) {
+            int size = baseSize * (int)Math.pow(2, i);
+            Ns.add(size);
+            opCounts.add(ops);
+            times.add(naiveNearest(size, ops));
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    private static double naiveNearest(int size, int ops) {
+        Random ranXY = new Random();
+        ArrayList<Point> npsList = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            npsList.add(new Point(ranXY.nextDouble(), ranXY.nextDouble()));
+        }
+        NaivePointSet nps = new NaivePointSet(npsList);
+        Stopwatch sw = new Stopwatch();
+        for(int i = 0; i < ops; i++) {
+            nps.nearest(ranXY.nextDouble(), ranXY.nextDouble());
+        }
+        double timeInSeconds = sw.elapsedTime();
+        return timeInSeconds;
+    }
+
+    private static void KDTNearestTime(int baseSize) {
+        int ops = 1000000;
+        List<Integer> Ns = new ArrayList<>();
+        List<Double> times = new ArrayList<>();
+        List<Integer> opCounts = new ArrayList<>();
+        for(int i = 0; i < 6; i++) {
+            int size = baseSize * (int)Math.pow(2, i);
+            Ns.add(size);
+            opCounts.add(ops);
+            times.add(KDTNearest(size, ops));
+        }
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    private static double KDTNearest(int size, int ops) {
+        Random ranXY = new Random();
+        ArrayList<Point> KDTList = new ArrayList<>();
+        for(int i = 0; i < size; i++) {
+            KDTList.add(new Point(ranXY.nextDouble(), ranXY.nextDouble()));
+        }
+        KDTree KDtree = new KDTree(KDTList);
+        Stopwatch sw = new Stopwatch();
+        for(int i = 0; i < ops; i++) {
+            KDtree.nearest(ranXY.nextDouble(), ranXY.nextDouble());
+        }
+        double timeInSeconds = sw.elapsedTime();
+        return timeInSeconds;
+    }
+
+    private static void findNearestP() {
+        Random ranXY = new Random();
+        ArrayList<Point> treeList = new ArrayList<>();
+        for(int i = 0; i < 1000000; i++) {
+            treeList.add(new Point(ranXY.nextDouble(),ranXY.nextDouble()));
+        }
+        KDTree treeTwo = new KDTree(treeList);
+        NaivePointSet nps = new NaivePointSet(treeList);
+        double goal_X = ranXY.nextDouble();
+        double goal_Y = ranXY.nextDouble();
+        Point goal = new Point(goal_X, goal_Y);
+        Point nearestP1 = treeTwo.nearest(goal_X, goal_Y);
+        Point nearestP2 = nps.nearest(goal_X, goal_Y);
+        try {
+            assertEquals(Point.distance(nearestP1, goal),
+                    Point.distance(nearestP2, goal), 0.00000001);
+            System.out.println("Success! The nearest point is: ");
+            System.out.println("KDTree result: ");
+            System.out.println(nearestP1);
+            System.out.println("NaivePointSet result: ");
+            System.out.println(nearestP2);
+        }
+        catch(AssertionError e) {
+            System.out.println("Fail!");
+            System.out.println("KDTree result: " + nearestP1);
+            System.out.println("NaivePointSet result: " + nearestP2);
+        }
+    }
+    /*
+    private static void test1() {
+        ArrayList<Point> treeList = new ArrayList<>();
+        treeList.add(new Point(2,3));
+        treeList.add(new Point(4,2));
+        treeList.add(new Point(4,5));
+        treeList.add(new Point(3,3));
+        treeList.add(new Point(1, 5));
+        treeList.add(new Point(4,4));
+        KDTree treeOne = new KDTree(treeList);
+        NaivePointSet nps = new NaivePointSet(treeList);
+        Point nearestP1 = treeOne.nearest(0, 7);
+        Point nearestP2 = nps.nearest(0, 7);
+        assertEquals(Point.distance(nearestP1, new Point(0, 7)),
+                Point.distance(nearestP2, new Point(0, 7)), 0);
+        //System.out.println(nearestP1);
+        //System.out.println(nearestP2);
+    }
+
+     */
 }
